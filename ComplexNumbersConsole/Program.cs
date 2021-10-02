8f057758547324a61291e0c308eb490a6a2c7856
CornerPin <7669876+CornerPin@users.noreply.github.com>
2021-10-02 14:39:20 +0300

    Console program.

--- ComplexNumbersConsole/Program.cs
+++ ComplexNumbersConsole/Program.cs
@@ -7,7 +7,36 @@ namespace ComplexNumbersConsole
 	{
 		static void Main(string[] args)
 		{
-			// TODO
+			var z = new Complex();
+			var w = new Complex();
+
+			Console.Write("Re(z) = ");
+			z.Real = double.Parse(Console.ReadLine());
+
+			Console.Write("Im(z) = ");
+			z.Imaginary = double.Parse(Console.ReadLine());
+
+			Console.WriteLine();
+
+			Console.Write("Re(w) = ");
+			w.Real = double.Parse(Console.ReadLine());
+
+			Console.Write("Im(w) = ");
+			w.Imaginary = double.Parse(Console.ReadLine());
+
+			Console.WriteLine();
+
+			Console.WriteLine("z + w = {0}", z + w);
+			Console.WriteLine("z - w = {0}", z - w);
+			Console.WriteLine("z * w = {0}", z * w);
+			Console.WriteLine("z / w = {0}", z / w);
+			Console.WriteLine("z ^ Re(w) = {0}", z.Pow(w.Real));
+			Console.WriteLine("|z| = {0}", z.Abs());
+			Console.WriteLine("Arg(z) = {0}", z.Arg());
+			Console.WriteLine("z* = {0}", z.Conjugate());
+			Console.WriteLine("Sqrt(z) = {0}", z.Sqrt());
+			Console.WriteLine("Log(z) = {0}", z.Log());
+			Console.WriteLine("Exp(z) = {0}", z.Exp());
 		}
 	}
 }
